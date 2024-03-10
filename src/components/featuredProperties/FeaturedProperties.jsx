72fd993431daf2ef1d4bd2233f9583aa26bcef63
Manupriyan B S <108547134+manu-git-b-s@users.noreply.github.com>
2024-03-10 13:25:52 +0530

    footer completed

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -32,7 +32,7 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg"
+          src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/294812676.jpg?k=d76a523be94c6a86fb359c8e95bb2628b36c06dba756caf112b0859ad579da30&o=&hp=1"
           alt=""
           className="fpImg"
         />
@@ -46,7 +46,7 @@ const FeaturedProperties = () => {
       </div>
       <div className="fpItem">
         <img
-          src="https://cf.bstatic.com/xdata/images/hotel/max1280x900/215955381.jpg?k=ff739d1d9e0c8e233f78ee3ced82743ef0355e925df8db7135d83b55a00ca07a&o=&hp=1"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTK_udEev9s5hdygiuE-O95jm3-gVGdqqA7qw&usqp=CAU"
           alt=""
           className="fpImg"
         />
